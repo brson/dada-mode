d1e83b5a0d0cb0647d5face7c18b885e431316d0
Brian Anderson <andersrb@gmail.com>
2025-03-24 22:17:07 -0600

    init

---
+++ dada-mode.el
@@ -0,0 +1,33 @@
+(define-generic-mode 'dada-mode
+  '("#") 
+  '(
+    "class"
+    "struct"
+    "fn"
+    "self"
+    "true"
+    "false"
+    "if"
+    "else"
+    "async"
+    "let"
+    "mut"
+    "print"
+    "leased"
+    "shared"
+    "return"
+    )
+  '(
+    ("bool" . 'font-lock-type-face)
+    ("u32" . 'font-lock-type-face)
+    ("+" . 'font-lock-operator-face)
+    ("*" . 'font-lock-operator-face)
+    (":" . 'font-lock-punctuation-face)
+    ("," . 'font-lock-delimiter-face)
+    ("(" . 'font-lock-bracket-face)
+    (")" . 'font-lock-bracket-face)
+    ("{" . 'font-lock-bracket-face)
+    ("}" . 'font-lock-bracket-face)
+    )
+  '(".dada\\'")
+  "dada language")
